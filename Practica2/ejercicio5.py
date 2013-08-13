023b4883430e1769e0e09106e6b458dd88ef4c14
elsuizo <martin_noblia@lavabit.com>
2013-08-13 13:15:11 -0300

    agrego ejercicio 4

---
+++ Practica2/ejercicio5.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+
+#*************************************************************************
+# Para dos Imagenes distintintas (en escala de grises) acentuar los bordes
+# utilizando los filtros “Laplacian Sharpening” y “Hight Boost Filter”
+#*************************************************************************
+
+#Imports
+#******************************************************************************
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#*****************************************************************************
+
+path1 = '/Imagenes/baboon.jpg'
