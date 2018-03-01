10c590b8e97296e87f2bd173e51cc0273210fed8
elsuizo <mnoblia@disroot.org>
2018-03-01 11:54:16 -0300

    test

--- Practica2/ejercicio3.py
+++ Practica2/ejercicio3.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #*****************************************************************************
 
+# paths to the images
 path1 = 'Imagenes/sci-2.bmp'
 path2 = 'Imagenes/baboon.jpg'
 
