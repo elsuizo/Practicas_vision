e5615133c57937db6859e1fd8acd754e6cbdc69a
elsuizo <mnoblia@disroot.org>
2019-07-15 13:05:51 -0300

    fix ej8 gaussian

--- Practica1/ejercicio8_gaussian.py
+++ Practica1/ejercicio8_gaussian.py
@@ -15,9 +15,9 @@ import matplotlib.pyplot as plt
 
 path3 = 'Imagenes/sci-4.bmp'
 
-img3 = cv2.imread(path3, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+img3 = cv2.imread(path3, cv2.IMREAD_GRAYSCALE)
 img3_bin = np.copy(img3)
-img3_bin = cv2.medianBlur(img3_bin, 5)
+img3_bin = cv2.medianBlur(img3_bin, 7)
 
 th3 = cv2.adaptiveThreshold(img3_bin, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
         cv2.THRESH_BINARY, 11, 2)
