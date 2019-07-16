2dc830277f06eb6a4296872f3bb84fbe0a9d3e9c
elsuizo <mnoblia@disroot.org>
2019-07-16 10:55:21 -0300

    update ejercicio4 practica3

--- Practica3/Python/ejercicio4.py
+++ Practica3/Python/ejercicio4.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #*************************************************************************
-#4. Idem anterior pero aplicando pasaaltos.
+# 4. Idem anterior pero aplicando pasaaltos.
 #*************************************************************************
 
 
@@ -17,7 +17,7 @@ from scipy import fftpack
 # Funciones
 #******************************************************************************
 
-def dftuv(M,N):
+def dftuv(M, N):
     """
     Funcion para realizar meshgrids de frecuencias(Gonzalez, Goods, pag:128)
     Parametros:
@@ -31,29 +31,29 @@ def dftuv(M,N):
     meshgrid ---> U, V
 
     """
-    u = np.arange(0,M)
-    v = np.arange(0,N)
-    
+    u = np.arange(0, M)
+    v = np.arange(0, N)
+
     idx = np.where(u >= M/2)
     u[idx] = u[idx] - M
     idy = np.where(v >= N/2)
     v[idy] = v[idy] - N
-    
-    V, U = np.meshgrid(v,u)
-    
-    return V,U
 
-def hpfilter_ideal(im,D_0):
+    V, U = np.meshgrid(v, u)
+
+    return V, U
+
+def hpfilter_ideal(im, D_0):
     """ """
     M,N = im.shape
-    U,V = dftuv(M,N)
-    
+    U,V = dftuv(M, N)
+
     D = np.sqrt(U**2 + V**2)
-    
+
     H_lp = (D <= D_0)
     H_hp = 1 - H_lp
     H_shift = np.fft.fftshift(H_hp)
-    
+
     F = fftpack.fft2(im)
     F_shift = np.fft.fftshift(F)
 
@@ -62,20 +62,20 @@ def hpfilter_ideal(im,D_0):
     im_back = np.fft.ifft2(im_fil)
 
     im_back = np.abs(im_back)
-    
+
     return im_back
 
-def hpfilter_butterworth(im,D_0,n):
+def hpfilter_butterworth(im, D_0, n):
     """ """
     M,N = im.shape
-    U,V = dftuv(M,N)
-    
+    U, V = dftuv(M, N)
+
     D = np.sqrt(U**2 + V**2)
-    
-    H_lp = 1/(1 + (D/D_0)**(2*n))
+
+    H_lp = 1 / (1 + (D / D_0)**(2*n))
     H_hp = 1 - H_lp
     H_shift = np.fft.fftshift(H_hp)
-    
+
     F = fftpack.fft2(im)
     F_shift = np.fft.fftshift(F)
 
@@ -84,21 +84,21 @@ def hpfilter_butterworth(im,D_0,n):
     im_back = np.fft.ifft2(im_fil)
 
     im_back = np.abs(im_back)
-    
+
     return im_back
 
-def hpfilter_gaussian(im,D_0):
+def hpfilter_gaussian(im, D_0):
     """ """
     M,N = im.shape
-    U,V = dftuv(M,N)
-    
+    U, V = dftuv(M, N)
+
     D = np.sqrt(U**2 + V**2)
-    #mascara gaussiana
+    # mascara gaussiana
     H_lp = np.exp((-D**2 )/ (2 * D_0**2))
     H_hp = 1 - H_lp
 
     H_shift = np.fft.fftshift(H_hp)
-    
+
     F = fftpack.fft2(im)
     F_shift = np.fft.fftshift(F)
 
@@ -107,13 +107,13 @@ def hpfilter_gaussian(im,D_0):
     im_back = np.fft.ifft2(im_fil)
 
     im_back = np.abs(im_back)
-    
-    return im_back
-#******************************************************************************
 
-#cargamos las Imagenes
-path1 = 'Imagenes/baboon.jpg'
-path2 = 'Imagenes/Rana2.bmp'
+    return im_back
+
+
+# cargamos las Imagenes
+path1 = '../Imagenes/baboon.jpg'
+path2 = '../Imagenes/Rana2.bmp'
 img1 = cv2.imread(path1, 0)
 img2 = cv2.imread(path2, 0)
 
@@ -129,8 +129,9 @@ img2_lp_butt = hpfilter_butterworth(img2,D_0,5)
 img2_lp_gauss = hpfilter_gaussian(img2,D_0)
 
 
-#******************************************************************************
-# Plots
+#-------------------------------------------------------------------------
+#                        plots
+#-------------------------------------------------------------------------
 plt.figure()
 plt.subplot(221),plt.imshow(img1, cmap = 'gray')
 plt.title('Imagen Original'), plt.xticks([]), plt.yticks([])
