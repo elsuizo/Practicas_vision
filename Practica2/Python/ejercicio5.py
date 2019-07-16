7bc1a02c12bc0f90f0d9c72889ff019befe8747c
elsuizo <mnoblia@disroot.org>
2019-07-16 10:12:13 -0300

    update ejercicio5 practica2

--- Practica2/Python/ejercicio5.py
+++ Practica2/Python/ejercicio5.py
@@ -13,8 +13,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 #*****************************************************************************
 
-path1 = 'Imagenes/baboon.jpg'
-path2 = 'Imagenes/Rana2.bmp'
+path1 = '../Imagenes/baboon.jpg'
+path2 = '../Imagenes/Rana2.bmp'
 
 img1 = cv2.imread(path1, 0)
 img2 = cv2.imread(path2, 0)
@@ -22,12 +22,15 @@ img2 = cv2.imread(path2, 0)
 #Utilizamos las herramientas de Opencv
 laplaciano = cv2.Laplacian(img2, -1)
 #Una alternativa es usar filter2d ingresando el kernel correspondiente
-ker_laplacian = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+ker_laplacian = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
 laplaciano2 = cv2.filter2D(img2, -1, ker_laplacian)
 
-ker_hb = lambda A: np.array([[0,-1,0],[-1,A+4,-1],[0,-1,0]])
+ker_hb = lambda A: np.array([[0, -1, 0], [-1, A+4, -1], [0, -1, 0]])
 img2_hb = cv2.filter2D(img2, -1, ker_hb(1))
 
+#-------------------------------------------------------------------------
+#                        plots
+#-------------------------------------------------------------------------
 plt.figure()
 plt.imshow(laplaciano, cmap='gray')
 plt.title('Laplaciano')
