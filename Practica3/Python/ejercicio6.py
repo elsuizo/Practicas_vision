3c7b41d8b70f7e8975726d2e1318a78ffd77147e
elsuizo <mnoblia@disroot.org>
2019-07-16 10:58:42 -0300

    update ejercicio5 practica3

--- Practica3/Python/ejercicio6.py
+++ Practica3/Python/ejercicio6.py
@@ -14,9 +14,10 @@ import matplotlib.pyplot as plt
 from scipy import fftpack
 #*****************************************************************************
 
+# TODO(elsuizo:2019-07-16): a esto habia que hacerlo con la fft y un patron
 
-path1 = 'Imagenes/baboon.jpg'
-path2 = 'Imagenes/ojo_baboon.png'
+path1 = '../Imagenes/baboon.jpg'
+path2 = '../Imagenes/ojo_baboon.png'
 img1 = cv2.imread(path2, 0)
 mask = cv2.imread(path2, 0)
 
