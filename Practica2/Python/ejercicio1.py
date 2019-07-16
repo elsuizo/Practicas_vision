37a2e3d1f00f796c1a0bf700f5a739679fceaf65
elsuizo <mnoblia@disroot.org>
2019-07-16 09:37:59 -0300

    update ejercicio1 practica2

--- Practica2/Python/ejercicio1.py
+++ Practica2/Python/ejercicio1.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 #*************************************************************************
-# Mostrar mediante ejempos los efectos que se producen en el histograma de 
-# imagen al modificarle su brillo y su contraste 
+# Mostrar mediante ejempos los efectos que se producen en el histograma de
+# imagen al modificarle su brillo y su contraste
 #*************************************************************************
 
 #Imports
@@ -19,16 +19,16 @@ def imadjust(imagen, low_in, hig_in, low_out, hig_out, gamma):
     """
     imagen_mod = np.zeros(imagen.shape, dtype=np.uint8)
 
-    imagen_mod = low_out+(hig_out-low_out)*((imagen-low_in)/(hig_in-low_in))**gamma
+    imagen_mod = low_out + (hig_out - low_out) * ((imagen - low_in) / (hig_in - low_in)) ** gamma
 
 
     return imagen_mod
 
 def histograma(imagen):
-    """Docstring de la funcion"""
+    """Funcion para calcular el histograma de una imagen"""
     h = np.zeros(256, dtype=float)
     imagen = imagen.astype(np.uint8)
-    for i in xrange(256):
+    for i in range(256):
         x = np.array([])
         x = np.argwhere(imagen== i)
         if (len(x) == 0):
@@ -36,15 +36,18 @@ def histograma(imagen):
         h[i] = len(x)/float(imagen.size)  # Normalizamos
     return h
 
-path1 = 'Imagenes/galaxy_gray.jpeg'
-path2 = 'Imagenes/baboon.jpg'
+path1 = '../Imagenes/galaxy2_gray.jpg'
+path2 = '../Imagenes/baboon.jpg'
 
-img1 = cv2.imread(path1, 0) 
+img1 = cv2.imread(path1, 0)
 J = imadjust(img1, .5, .75, .3, 1, .3)
-print J
+print(J)
 hist1 = histograma(img1)
 hist2 = histograma(J)
 
+#-------------------------------------------------------------------------
+#                        plots
+#-------------------------------------------------------------------------
 plt.figure()
 plt.imshow(J, cmap='gray')
 plt.figure()
@@ -54,4 +57,3 @@ plt.stem(np.arange(256), hist1, linefmt='b-', markerfmt='bo', basefmt='r-')
 plt.figure()
 plt.stem(np.arange(256), hist2, linefmt='b-', markerfmt='bo', basefmt='r-')
 plt.show()
-
