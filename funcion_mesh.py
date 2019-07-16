3a97327e4123b4f6319b4fc0aae58346a2014733
elsuizo <mnoblia@disroot.org>
2019-07-16 08:40:26 -0300

    no se para que era esta funcion pero la dejo

--- funcion_mesh.py
+++ funcion_mesh.py
@@ -1,31 +1,26 @@
-def dftuv(M,N):
+def dftuv(M, N):
     """
     Funcion para realizar meshgrids de frecuencias(Gonzalez, Goods, pag:128)
     Parametros:
     ------------------------------------------------------------------------
     M : entero
         Dimension en U
-        
+
     N : entero
         Dimension en V
-    
+
     Salida
     ------------------------------------------------------------------------
-    meshgrid ---> U, V 
-    
-
-    
+    meshgrid ---> U, V
     """
-    u = np.arange(0,M)
-    v = np.arange(0,N)
-    
-    idx = np.where(u > M/2)
+    u = np.arange(0, M)
+    v = np.arange(0, N)
+
+    idx = np.where(u > M / 2)
     u[idx] = u[idx] - M
-    idy = np.where(v > N/2)
+    idy = np.where(v > N / 2)
     v[idy] = v[idy] - N
-    
-    V, U = np.meshgrid(v,u)
-    
-    return V,U
-    
-        
+
+    V, U = np.meshgrid(v, u)
+
+    return V, U
